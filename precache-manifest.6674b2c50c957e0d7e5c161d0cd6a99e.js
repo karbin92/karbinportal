4b03c2875ba6253044c0ef67a233ffa86df43b59
Karbin92 <fredrik.karlsson1992@live.com>
2019-06-24 20:55:54 +0200

    Updates

---
+++ precache-manifest.6674b2c50c957e0d7e5c161d0cd6a99e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c045585823b255cab9e077724d12ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe398cbaee671315c87",
+    "url": "/static/css/main.08f78ba0.chunk.css"
+  },
+  {
+    "revision": "8b22767f196583cc2bab",
+    "url": "/static/js/2.53c72fde.chunk.js"
+  },
+  {
+    "revision": "3fe398cbaee671315c87",
+    "url": "/static/js/main.4ed7328b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
